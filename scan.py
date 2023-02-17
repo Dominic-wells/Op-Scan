14d876004db86a5d959d1985577e66753c024b36
Dominic-wells <joedomwells@gmail.com>
2023-02-17 17:16:30 +0000

    Not working. Added new func to scan select ports

--- scan.py
+++ scan.py
@@ -1,6 +1,9 @@
 import socket
 
-def scan(ip, port):
+
+# This function will scan the port and return the status of the ports 1,1025 (open or closed)
+# guidence from https://docs.python.org/3/library/socket.html
+def scanAll(ip, port):
     try:
         sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(0.5)
@@ -15,4 +18,68 @@ def scan(ip, port):
 
 host = input("Enter host to scan: ")
 for port in range(1,1025):
-    scan(host, port)
+    scanAll(host, port)
+    
+    
+    
+#this function will scan the most vulnerable ports and return the status of the
+# ports 20,21,23,25,53,80,137,139,443,1433,1434,3306,3389,8080,8443 (open or closed)
+# guidence from https://blog.netwrix.com/2022/08/04/open-port-vulnerabilities-list/#:~:text=Ports%2080%2C%20443%2C%208080%20and,request%20forgeries%20and%20DDoS%20attacks.   
+def scanMostVulnerable(ip,port):
+    try:
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        results=sock.connect_ex((ip, port))
+        if results == 0:
+            print ("Port {}: Open".format(port))
+        else:
+            print ("Port {}: Closed".format(port))
+        sock.close()
+    except:
+        print("unable to connect to host {}".format(ip))
+
+host = input("Enter host to scan: ")
+ports = [20,21,23,25,53,80,137,139,443,1433,1434,3306,3389,8080,8443]
+for port in ports:
+    scanMostVulnerable(host, port)
+    
+def main():
+    while True:
+        print("1. Scan all ports")
+        print("2. Scan most vulnerable ports")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            scanAll(host,port)
+        elif choice == "2":
+            scanMostVulnerable(host,port)
+        elif choice == "3":
+            print ("Goodbye,See you next time")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    print("Welcome to the Op-scanner")
+    main()
+    
+    
+def main():
+    while True:
+        print("1. Scan all ports")
+        print("2. Scan most vulnerable ports")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            scanAll(host,port)
+        elif choice == "2":
+            scanMostVulnerable(host,port)
+        elif choice == "3":
+            print ("Goodbye,See you next time")
+            break
+        else:
+            print("Invalid choice")   
+    
+if __name__ == "__main__":
+    print("Welcome to the Op-scanner")
+    main()
